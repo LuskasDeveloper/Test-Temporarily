542fe15b482ff0b9dafbf7dd649dd5a70baf20bd
Luskas <algumacoisakk.7@gmail.com>
2025-08-03 03:05:11 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,26 +1,31 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+-- Library
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
+-- Window
 local Window = redzlib:MakeWindow({
   Title = "redz Hub : Blox Fruits",
   SubTitle = "by redz9999",
   SaveFolder = "testando | redz lib v5.lua"
 })
 
-Window:SelectTab(Tab1)
-
+-- Minimize Button
 Window:AddMinimizeButton({
     Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
-Tab1:AddDiscordInvite({
-    Name = "Name Hub",
-    Description = "Join server",
+-- Tab : Information
+local Information = Window:MakeTab({"Information", "Home"})
+
+Information:AddDiscordInvite({
+    Name = "Luck1ng Team",
+    Description = "Join Server",
     Logo = "rbxassetid://18751483361",
-    Invite = "Link discord invite",
+    Invite = "https://discord.gg/0",
 })
 
-local Tab1 = Window:MakeTab({"Um", "cherry"})
+-- Tab : Troll Player
+local Tab1 = Window:MakeTab({"Troll Player", "Home"})
 
 local Section = Tab1:AddSection({"Section"})
 
